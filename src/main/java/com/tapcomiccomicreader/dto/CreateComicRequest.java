f2a466fac572613f64932fc64872c281a0f6a5fd
P <Thanachot.so@kkumail.com>
2026-02-15 18:31:57 +0700

    add DTO for creating comic

--- src/main/java/com/tapcomiccomicreader/dto/CreateComicRequest.java
+++ src/main/java/com/tapcomiccomicreader/dto/CreateComicRequest.java
@@ -1,4 +1,56 @@
 package com.tapcomiccomicreader.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateComicRequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    private String author;
+    private String artist;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateComicRequest{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", author='" + author + '\'' +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
 }
