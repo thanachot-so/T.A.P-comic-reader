de34a758c4451473be486c1331492636d7c9cc07
P <Thanachot.so@kkumail.com>
2026-02-16 23:42:09 +0700

    add create at field and remove page entity from json

--- src/main/java/com/tapcomiccomicreader/entity/Chapter.java
+++ src/main/java/com/tapcomiccomicreader/entity/Chapter.java
@@ -1,9 +1,13 @@
 package com.tapcomiccomicreader.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import org.hibernate.annotations.Formula;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +32,20 @@ public class Chapter {
 
     @OneToMany(mappedBy = "chapter", cascade = CascadeType.ALL)
     @JsonManagedReference
+    @JsonIgnore
     private List<Page> pages;
+
+    @Formula("(SELECT COUNT(*) FROM Page p WHERE p.chapter_id = id)")
+    private Integer pageCount;
+
+    @Column(name = "create_at")
+    @JsonFormat
+    private LocalDateTime createAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createAt = LocalDateTime.now();
+    }
 
     public Chapter() {
         this.pages = new ArrayList<>();
@@ -79,6 +96,22 @@ public class Chapter {
         this.count = count;
     }
 
+    public Integer getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(Integer pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
     @Override
     public String toString() {
         return "Chapter{" +
@@ -87,6 +120,8 @@ public class Chapter {
                 ", count=" + count +
                 ", comic=" + comic +
                 ", pages=" + pages +
+                ", pageCount=" + pageCount +
+                ", createAt=" + createAt +
                 '}';
     }
 }
