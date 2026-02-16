2f1ceeff9835f8d1bba9ac8157a0d053a05024b6
P <Thanachot.so@kkumail.com>
2026-02-16 23:44:12 +0700

    push :D

--- src/main/java/com/tapcomiccomicreader/rest/ChapterRestController.java
+++ src/main/java/com/tapcomiccomicreader/rest/ChapterRestController.java
@@ -1,4 +1,34 @@
 package com.tapcomiccomicreader.rest;
 
+import com.tapcomiccomicreader.service.ChapterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comics")
 public class ChapterRestController {
+    private final ChapterService chapterService;
+
+    @Autowired
+    public ChapterRestController(ChapterService chapterService) {
+        this.chapterService = chapterService;
+    }
+
+    @GetMapping("/{id}/chapter")
+    public ResponseEntity<Object> getChapters(@PathVariable int id) {
+        return ResponseEntity.ok(chapterService.findAll(id));
+    }
+
+    @PostMapping("/{id}/chapter")
+    public ResponseEntity<String> uploadChapter(
+            @PathVariable int id,
+            @RequestParam("file") List<MultipartFile> files) throws IOException {
+        chapterService.uploadChapter(id, files);
+        return ResponseEntity.ok("file upload successfully");
+    }
 }
--- src/main/java/com/tapcomiccomicreader/rest/PageRestController.java
+++ src/main/java/com/tapcomiccomicreader/rest/PageRestController.java
@@ -1,4 +1,53 @@
 package com.tapcomiccomicreader.rest;
 
+import com.tapcomiccomicreader.dto.PageDTO;
+import com.tapcomiccomicreader.service.ComicService;
+import com.tapcomiccomicreader.service.PageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/comics")
 public class PageRestController {
+    private final ComicService comicService;
+    private final PageService pageService;
+
+
+    @Autowired
+    public PageRestController(ComicService comicService, PageService pageService) {
+        this.comicService = comicService;
+        this.pageService = pageService;
+    }
+
+    @GetMapping("/{comicUuid}/{chapterNum}/{pageNum}")
+    public ResponseEntity<PageDTO> findComicPage(@PathVariable String comicUuid,
+                                                 @PathVariable int chapterNum,
+                                                 @PathVariable int pageNum) {
+        var comic = comicService.findByUuid(comicUuid);
+        var page = pageService.find(comic.getId(), chapterNum, pageNum);
+        return ResponseEntity.ok(new PageDTO(page));
+    }
+
+    @PutMapping("/{id}/{chapter}/{page}")
+    public ResponseEntity<String> replacePageFile(
+            @RequestParam("file") MultipartFile file,
+            @PathVariable int id,
+            @PathVariable int chapter,
+            @PathVariable int page) throws IOException {
+        pageService.replacePage(id, chapter, page, file);
+        return ResponseEntity.ok("file upload successfully");
+    }
+
+    @DeleteMapping("/{id}/{chapter}/{page}")
+    public ResponseEntity<String> deletePageFile(
+            @PathVariable int id,
+            @PathVariable int chapter,
+            @PathVariable int page) throws IOException {
+        pageService.remove(id, chapter, page);
+        return ResponseEntity.ok("page deleted successfully");
+    }
 }
