5ef587d0e385da2d41474ed5ab0e056fad106d36
P <Thanachot.so@kkumail.com>
2026-02-19 13:18:40 +0700

    temporary DTO for login concept

---
+++ src/main/java/com/tapcomiccomicreader/dto/LoginRequest.java
@@ -0,0 +1,36 @@
+package com.tapcomiccomicreader.dto;
+//temporary DTO
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
