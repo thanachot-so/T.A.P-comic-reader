52a82ba5ef2f652a4d5b7cd46cec085be6ef51f8
P <Thanachot.so@kkumail.com>
2026-02-19 13:16:59 +0700

    implement remove, reupload, and upload in sequence method and refactoring the code

--- src/main/java/com/tapcomiccomicreader/service/ChapterServiceImpl.java
+++ src/main/java/com/tapcomiccomicreader/service/ChapterServiceImpl.java
@@ -10,6 +10,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,12 +18,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
 public class ChapterServiceImpl implements ChapterService{
+    private final PageService pageService;
     @Value("${app.comic.location:./comics}")
     private String storageDirectory;
 
@@ -33,10 +36,11 @@ public class ChapterServiceImpl implements ChapterService{
 
 
     @Autowired
-    public ChapterServiceImpl(ChapterRepository chapterRepository, PageRepository pageRepository, ComicService comicService) {
+    public ChapterServiceImpl(ChapterRepository chapterRepository, PageRepository pageRepository, ComicService comicService, PageService pageService) {
         this.chapterRepository = chapterRepository;
         this.pageRepository = pageRepository;
         this.comicService = comicService;
+        this.pageService = pageService;
     }
 
     @PostConstruct
@@ -68,15 +72,8 @@ public class ChapterServiceImpl implements ChapterService{
     }
 
     @Override
-    public void uploadChapter(int comicId, List<MultipartFile> pages) throws IOException {
-//        sorting file by number in file name by ascending order
-        pages.sort((f1,f2) -> {
-            int n1 = extractNumber(f1.getOriginalFilename());
-            int n2 = extractNumber(f2.getOriginalFilename());
-
-            return Integer.compare(n1, n2);
-        });
-
+    @Transactional
+    public void uploadChapterInSequence(int comicId, List<MultipartFile> pages) throws IOException {
 //        get the comic by id from parameter
         Comic comic = comicService.find(comicId);
 
@@ -91,40 +88,78 @@ public class ChapterServiceImpl implements ChapterService{
 
         chapterRepository.save(newChapter);
 
+        savePages(pages, newChapter);
+    }
+
+    @Override
+    @Transactional
+    public void reUploadChapter(int comicId, int chapterNum, List<MultipartFile> pages) throws IOException {
+        var chapter = chapterRepository.findByChapterNum(comicId, chapterNum)
+                .orElseThrow(() -> new ResourceNotFoundException("could not find the chapter - " + chapterNum));
+
+//        remove old pages
+        List<Page> oldPages = new ArrayList<>(chapter.getPages());
+        chapter.getPages().clear();
+        for (Page page : oldPages) {
+            pageService.remove(page.getId());
+        }
+
+        savePages(pages, chapter);
+    }
+
+    @Override
+    @Transactional
+    public void remove(int id) throws IOException{
+        var chapter = find(id);
+
+        for (var page : chapter.getPages()) {
+            pageService.remove(page.getId());
+        }
+
+        chapterRepository.delete(chapter);
+    }
+
+    private void savePages(List<MultipartFile> pages, Chapter chapter) throws IOException{
+        int comicId = chapter.getComic().getId();
+        int chapterNum = chapter.getCount();
+        String comicUuid = chapter.getComic().getUuid();
+
+        sortPages(pages);
+
 //        page name and upload path
-        String pageName = comic.getUuid() +"_"+ newChapterNumber +"_";
-        String chapterUploadPath = storageDirectory +"/comics/"+ comicId +"/Chapters/ch-"+ newChapterNumber +"/";
+        String pageName = comicUuid +"_"+ chapterNum +"_";
+        String UploadPath = storageDirectory +"/comics/"+ comicId +"/Chapters/ch-"+ chapterNum +"/";
+
+//        url relative path
+        String relativePath = "/images/comics/"+ comicId + "/chapters/ch-" + chapterNum + "/";
 
 //        create new directory if the path doesn't exist
-        Path chapterDir = Paths.get(chapterUploadPath);
+        Path chapterDir = Paths.get(UploadPath);
         if (!Files.exists(chapterDir)) {
             Files.createDirectories(chapterDir);
         }
 
 //        uploading logic
         int pageCounter = 1;
-
         for (MultipartFile file : pages) {
 //            getting file format
             String originalFileName = file.getOriginalFilename();
-            String fileFormat = originalFileName.substring(originalFileName.lastIndexOf("."));
+            String fileSuffix = originalFileName.substring(originalFileName.lastIndexOf("."));
 
 //            creating file name with page number and file format
-            String fileName = "page_" + pageCounter + fileFormat;
+            String finalFileName = "page_" + pageCounter + fileSuffix;
 
-//            copy the uploaded file to the directory
-            Path filePath = Paths.get(chapterUploadPath + pageName + fileName);
-            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+//            copy the uploaded file to the local directory
+            Path targetPath = Paths.get(UploadPath + pageName + finalFileName);
+            Files.copy(file.getInputStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
 
 //            initiate the page entity and link it to chapter entity
             Page newPage = new Page();
             newPage.setCount(pageCounter);
-            newPage.setChapter(newChapter);
+            newPage.setChapter(chapter);
 
 //            file url for fetcher to get
-            String fileUrl = "/images/comics/"+ comicId +
-                    "/chapters/ch-" + newChapterNumber +
-                    "/"+ pageName + fileName;
+            String fileUrl =  relativePath + pageName + finalFileName;
 
 //            saving the data
             newPage.setUrl(fileUrl);
@@ -135,7 +170,17 @@ public class ChapterServiceImpl implements ChapterService{
         }
     }
 
-    public int extractNumber(String fileName) {
+    private void sortPages(List<MultipartFile> pages) {
+        //        sorting file by number in file name by ascending order
+        pages.sort((f1,f2) -> {
+            int n1 = extractNumber(f1.getOriginalFilename());
+            int n2 = extractNumber(f2.getOriginalFilename());
+
+            return Integer.compare(n1, n2);
+        });
+    }
+
+    private int extractNumber(String fileName) {
         if (fileName == null) {return Integer.MAX_VALUE;}
 
         Matcher matcher = NUMBER_PATTERN.matcher(fileName);
