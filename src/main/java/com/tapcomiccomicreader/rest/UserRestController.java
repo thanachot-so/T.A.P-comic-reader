64f7d2c6ebc04284aec37f31aeb1a10742d46b50
P <Thanachot.so@kkumail.com>
2026-02-20 15:22:50 +0700

    add following comic and login mapping and refactoring

--- src/main/java/com/tapcomiccomicreader/rest/UserRestController.java
+++ src/main/java/com/tapcomiccomicreader/rest/UserRestController.java
@@ -2,6 +2,8 @@ package com.tapcomiccomicreader.rest;
 
 import com.tapcomiccomicreader.dto.AddFriendRequest;
 import com.tapcomiccomicreader.dto.CreateUserRequest;
+import com.tapcomiccomicreader.dto.FavoriteComicRequest;
+import com.tapcomiccomicreader.dto.LoginRequest;
 import com.tapcomiccomicreader.entity.User;
 import com.tapcomiccomicreader.service.UserService;
 import jakarta.validation.Valid;
@@ -10,9 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserRestController {
     private final UserService userService;
 
@@ -21,23 +24,23 @@ public class UserRestController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> findAll() {
         return userService.findAll();
     }
 
-    @GetMapping("/users/{userUuid}")
+    @GetMapping("/{userUuid}")
     public User getUserByUUID(@PathVariable String userUuid) {
         return userService.findByUuid(userUuid);
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody @Valid CreateUserRequest request) {
         userService.save(new User(request.getName(), request.getPassword()));
         return ResponseEntity.ok("user: " + request.getName() + " has been created");
     }
 
-    @PostMapping("/users/add")
+    @PostMapping("/add")
     public String addUser(@RequestBody @Valid AddFriendRequest friendRequest) {
         var user = userService.findByUuid(friendRequest.getUserUuid());
         var friend = userService.findByUuid(friendRequest.getFriendUuid());
@@ -50,9 +53,20 @@ public class UserRestController {
         }
     }
 
-    @DeleteMapping("/users/{userId}")
+    @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteUser(@PathVariable int userId) {
         userService.deleteById(userId);
         return ResponseEntity.ok("user id : " + userId + " has been deleted");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Boolean>> login(@RequestBody @Valid LoginRequest request) {
+        return userService.login(request);
+    }
+
+    @PostMapping("/follow")
+    public ResponseEntity<Object> favorite(@RequestBody @Valid FavoriteComicRequest request) {
+        userService.favorite(request);
+        return ResponseEntity.ok("request success");
     }
 }
