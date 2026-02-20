a1f6b84decf79a63bcb1533e5290f7ee0d2aac92
P <Thanachot.so@kkumail.com>
2026-02-20 15:21:44 +0700

    add following comic repo logic

--- src/main/java/com/tapcomiccomicreader/dao/UserRepository.java
+++ src/main/java/com/tapcomiccomicreader/dao/UserRepository.java
@@ -2,10 +2,37 @@ package com.tapcomiccomicreader.dao;
 
 import com.tapcomiccomicreader.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findByUuid(String uuid);
+    @Query("SELECT u FROM User u " +
+            "WHERE u.uuid = :uuid")
+    Optional<User> findByUuid(@Param("uuid") String uuid);
+
+    @Query("SELECT u FROM User u " +
+            "WHERE u.name = :name " +
+            "AND u.password = :password")
+    Optional<User> login(@Param("name") String name,
+                         @Param("password") String password);
+
+    @Query(value = "SELECT COUNT (*) > 0 FROM user_follows WHERE user_id = :userId AND comic_id = :comicId", nativeQuery = true)
+    boolean isFollowed(@Param("userId") int userId,
+                       @Param("comicId") int comicId);
+
+    @Modifying
+    @Query(value = "INSERT INTO user_follows (user_id, comic_id) VALUES (:userId, :comicId)", nativeQuery = true)
+    void followComic(@Param("userId") int userId,
+                     @Param("comicId") int comicId);
+
+    @Modifying
+    @Query(value = "DELETE FROM user_follows WHERE user_id = :userId AND comic_id = :comicId", nativeQuery = true)
+    void removeFollowed(@Param("userId") int userId,
+                        @Param("comicId") int comicId);
+
 
 }
