b49ff95c384d3df37c6917b1cee9b3e073eacbb9
P <Thanachot.so@kkumail.com>
2026-02-21 16:54:46 +0700

    add search method

--- src/main/java/com/tapcomiccomicreader/service/ComicService.java
+++ src/main/java/com/tapcomiccomicreader/service/ComicService.java
@@ -2,6 +2,7 @@ package com.tapcomiccomicreader.service;
 
 import com.tapcomiccomicreader.dto.UpdateComicRequest;
 import com.tapcomiccomicreader.entity.Comic;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,6 +18,8 @@ public interface ComicService {
     Comic find(int id);
 
     Comic findByUuid(String comicUuid);
+
+    Page<Comic> search(String keyword, int pageNumber);
 
     void uploadCover(int comicId, MultipartFile file) throws IOException;
 
