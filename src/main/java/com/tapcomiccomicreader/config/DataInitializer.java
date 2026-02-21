de127c5a3dc2c8fad7571754d58f0a2a0d18889f
P <Thanachot.so@kkumail.com>
2026-02-21 16:53:27 +0700

    add extension pg_trgm initializer

--- src/main/java/com/tapcomiccomicreader/config/DataInitializer.java
+++ src/main/java/com/tapcomiccomicreader/config/DataInitializer.java
@@ -4,18 +4,28 @@ import com.tapcomiccomicreader.service.ComicService;
 import com.tapcomiccomicreader.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class DataInitializer {
-    private UserService userService;
-    private ComicService comicService;
+    private final UserService userService;
+    private final ComicService comicService;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public DataInitializer(UserService userService, ComicService comicService) {
+    public DataInitializer(UserService userService, ComicService comicService, JdbcTemplate jdbcTemplate) {
         this.userService = userService;
         this.comicService = comicService;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void enableExtensions() {
+        jdbcTemplate.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm;");
     }
 
     @Bean
