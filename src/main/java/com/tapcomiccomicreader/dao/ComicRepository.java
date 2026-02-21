a967bd2cf47637d79eca36ff60c9f3de1c5e0d2e
P <Thanachot.so@kkumail.com>
2026-02-21 16:55:05 +0700

    add search query

--- src/main/java/com/tapcomiccomicreader/dao/ComicRepository.java
+++ src/main/java/com/tapcomiccomicreader/dao/ComicRepository.java
@@ -1,6 +1,8 @@
 package com.tapcomiccomicreader.dao;
 
 import com.tapcomiccomicreader.entity.Comic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,11 @@ public interface ComicRepository  extends JpaRepository<Comic,Integer> {
     @Query("SELECT c FROM Comic c " +
             "WHERE c.uuid = :uuid")
     Optional<Comic> findComicByUuid(@Param("uuid") String uuid);
+
+    @Query(value = "SELECT *, " +
+            "(SELECT COUNT(*) FROM user_follows uf WHERE uf.comic_id = id ) AS followerCount, " +
+            "(SELECT COUNT(*) FROM comic_chapter cc WHERE cc.comic_id = id) AS chapterCount " +
+            "FROM comic WHERE title ILIKE '%' || :keyword || '%' OR title % :keyword " +
+            "ORDER BY similarity(title, :keyword) DESC", nativeQuery = true)
+    Page<Comic> searchByTitle(@Param("keyword") String keyword, Pageable pageable);
 }
