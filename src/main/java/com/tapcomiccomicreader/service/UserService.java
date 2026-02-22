179d101b0c4ad59bcfdecc37c79d79e35ea913f0
P <Thanachot.so@kkumail.com>
2026-02-22 21:27:04 +0700

    add search method

--- src/main/java/com/tapcomiccomicreader/service/UserService.java
+++ src/main/java/com/tapcomiccomicreader/service/UserService.java
@@ -2,7 +2,9 @@ package com.tapcomiccomicreader.service;
 
 import com.tapcomiccomicreader.dto.FavoriteComicRequest;
 import com.tapcomiccomicreader.dto.LoginRequest;
+import com.tapcomiccomicreader.dto.UserDTO;
 import com.tapcomiccomicreader.entity.User;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -26,4 +28,6 @@ public interface UserService{
     ResponseEntity<Map<String, Boolean>> login(LoginRequest request);
 
     void favorite(FavoriteComicRequest request);
+
+    Page<UserDTO> search(String keyword, int pageNumber);
 }
