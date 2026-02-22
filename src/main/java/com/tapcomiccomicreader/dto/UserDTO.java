c4125d256a247880f9f49ff90ccf71a13488e74e
P <Thanachot.so@kkumail.com>
2026-02-22 21:25:59 +0700

    DTO for user data

---
+++ src/main/java/com/tapcomiccomicreader/dto/UserDTO.java
@@ -0,0 +1,59 @@
+package com.tapcomiccomicreader.dto;
+
+import com.tapcomiccomicreader.entity.User;
+
+public class UserDTO {
+    private String name;
+    private String uuid;
+    private Integer followedComicCount;
+    private Integer friends;
+
+    public UserDTO(User user) {
+        this.name = user.getName();
+        this.uuid = user.getUuid();
+        this.followedComicCount = user.getFollowedComicsCount();
+        this.friends = user.getFriends();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Integer getFollowedComicCount() {
+        return followedComicCount;
+    }
+
+    public void setFollowedComicCount(Integer followedComicCount) {
+        this.followedComicCount = followedComicCount;
+    }
+
+    public Integer getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Integer friends) {
+        this.friends = friends;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "name='" + name + '\'' +
+                ", uuid='" + uuid + '\'' +
+                ", followedComicCount=" + followedComicCount +
+                ", friends=" + friends +
+                '}';
+    }
+}
