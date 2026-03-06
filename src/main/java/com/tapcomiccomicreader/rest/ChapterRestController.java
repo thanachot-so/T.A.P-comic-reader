eddce2555cfeee4994aadcc60ad1b4c14ff93cba
P <Thanachot.so@kkumail.com>
2026-03-06 17:49:37 +0700

    add get mapping for list of pages

--- src/main/java/com/tapcomiccomicreader/rest/ChapterRestController.java
+++ src/main/java/com/tapcomiccomicreader/rest/ChapterRestController.java
@@ -1,7 +1,9 @@
 package com.tapcomiccomicreader.rest;
 
+import com.tapcomiccomicreader.dto.PageDTO;
 import com.tapcomiccomicreader.service.ChapterService;
 import com.tapcomiccomicreader.service.ComicService;
+import com.tapcomiccomicreader.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +17,26 @@ import java.util.List;
 public class ChapterRestController {
     private final ChapterService chapterService;
     private final ComicService comicService;
+    private final PageService pageService;
 
     @Autowired
-    public ChapterRestController(ChapterService chapterService, ComicService comicService) {
+    public ChapterRestController(ChapterService chapterService, ComicService comicService, PageService pageService) {
         this.chapterService = chapterService;
         this.comicService = comicService;
+        this.pageService = pageService;
     }
 
     @GetMapping("/{comicUuid}/chapter")
     public ResponseEntity<Object> getChapters(@PathVariable String comicUuid) {
         var comic = comicService.findByUuid(comicUuid);
         return ResponseEntity.ok(chapterService.findAll(comic.getId()));
+    }
+
+    @GetMapping("/{comicUuid}/{chapterNum}")
+    public ResponseEntity<List<PageDTO>> findChapterPages(@PathVariable String comicUuid,
+                                                          @PathVariable int chapterNum) {
+
+        return ResponseEntity.ok(pageService.findByComicUuidAndChapterNum(comicUuid, chapterNum));
     }
 
     @PostMapping("/{comicId}/chapter")
