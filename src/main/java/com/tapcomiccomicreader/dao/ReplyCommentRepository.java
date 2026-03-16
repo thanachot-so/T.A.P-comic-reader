fc14fc07a59969eb08abe66eeb916fe407e13f1d
P <Thanachot.so@kkumail.com>
2026-03-16 23:26:43 +0700

    add find by main comment id query

--- src/main/java/com/tapcomiccomicreader/dao/ReplyCommentRepository.java
+++ src/main/java/com/tapcomiccomicreader/dao/ReplyCommentRepository.java
@@ -2,7 +2,11 @@ package com.tapcomiccomicreader.dao;
 
 import com.tapcomiccomicreader.entity.ReplyComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReplyCommentRepository extends JpaRepository<ReplyComment, Integer> {
-
+    @Query("SELECT r FROM ReplyComment r WHERE r.mainComment.id = :parentId")
+    List<ReplyComment> findByParentId(int parentId);
 }
