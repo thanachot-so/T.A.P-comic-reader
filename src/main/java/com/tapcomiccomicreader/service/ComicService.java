48170c16206f2face17885f3cf97c0ec50afc071
P <Thanachot.so@kkumail.com>
2026-03-29 12:47:04 +0700

    add search comic by genre and set comic genre

--- src/main/java/com/tapcomiccomicreader/service/ComicService.java
+++ src/main/java/com/tapcomiccomicreader/service/ComicService.java
@@ -3,11 +3,13 @@ package com.tapcomiccomicreader.service;
 import com.tapcomiccomicreader.dto.ComicDTO;
 import com.tapcomiccomicreader.dto.UpdateComicRequest;
 import com.tapcomiccomicreader.entity.Comic;
+import com.tapcomiccomicreader.entity.Genre;
 import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 public interface ComicService {
     void save(Comic comic);
@@ -21,6 +23,12 @@ public interface ComicService {
     Comic findByUuid(String comicUuid);
 
     Page<ComicDTO> search(String keyword, int pageNumber);
+
+    Page<ComicDTO> searchByGenre(Set<Integer> genreIds, int pageNumber);
+
+    Set<Genre> getGenresByComicUuid(String comicUuid);
+
+    void setComicGenres(String comicUuid, Set<Integer> genreIds);
 
     void uploadCover(int comicId, MultipartFile file) throws IOException;
 
