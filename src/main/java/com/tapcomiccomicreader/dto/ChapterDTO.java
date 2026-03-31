7385c57d6824a87d7fde025c5c601cbabdaa4a56
P <Thanachot.so@kkumail.com>
2026-03-31 18:20:46 +0700

    add constructor for easier class initiating

--- src/main/java/com/tapcomiccomicreader/dto/ChapterDTO.java
+++ src/main/java/com/tapcomiccomicreader/dto/ChapterDTO.java
@@ -1,6 +1,7 @@
 package com.tapcomiccomicreader.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.tapcomiccomicreader.entity.Chapter;
 
 import java.time.LocalDateTime;
 
@@ -15,4 +16,17 @@ public record ChapterDTO(
 
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime createAt
-) {}
+) {
+        public ChapterDTO(Chapter chapter, Boolean currentUserVote) {
+                this(
+                        chapter.getId(),
+                        chapter.getName(),
+                        chapter.getCount(),
+                        chapter.getPageCount(),
+                        chapter.getLikeCount(),
+                        chapter.getDislikeCount(),
+                        currentUserVote,
+                        chapter.getCreateAt()
+                );
+        }
+}
