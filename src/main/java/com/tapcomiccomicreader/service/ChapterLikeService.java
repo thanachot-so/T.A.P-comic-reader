ec6a57b64566a7068b8591fc1a184126f4789f7c
P <Thanachot.so@kkumail.com>
2026-03-31 18:22:39 +0700

    add vote method

--- src/main/java/com/tapcomiccomicreader/service/ChapterLikeService.java
+++ src/main/java/com/tapcomiccomicreader/service/ChapterLikeService.java
@@ -2,8 +2,11 @@ package com.tapcomiccomicreader.service;
 
 import com.tapcomiccomicreader.entity.ChapterLike;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ChapterLikeService {
     void save(ChapterLike chapterLike);
-
     void vote(int userId, int chapterId, boolean vote);
+    Map<Integer, Boolean> getUserVotes(int userId, List<Integer> chapterIds);
 }
